54eb21888b2eac741177ae224f257bf17a1d4a66
jeff <b04901014@ntu.edu.tw>
2017-01-04 16:40:56 +0800

    delete unused

--- final/src/get_dict.py
+++
@@ -1,6 +0,0 @@
-from util import get_token, clean_html
-data = pd.read_csv("../data/test.csv")
-data = data.content.values.tolist()
-data = clean_html(data)
-data = [re.sub(r'\n', ' ', x) for x in data]
-data = [get_token(x) for x in data]
